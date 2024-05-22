60106829705271875507437e46012de35f9e35e4
kevinmoralesc <kevina.moralesc@uqvirtual.edu.co>
2024-05-21 22:48:19 -0500

    correcion 4

--- smoke_test.js
+++ smoke_test.js
@@ -33,6 +33,42 @@ async function handleAlertLogin(driver) {
     }
 }
 
+async function handleAlertRegistro(driver) {
+    try {
+        await driver.wait(until.elementLocated(By.id('swal2-html-container')), 10000);
+        let alertElement = await driver.findElement(By.id('swal2-html-container'));
+        let alertText = await alertElement.getText();
+        console.log('Registro con datos Correctos:');
+        console.log('Texto de la alerta:', alertText);
+
+        if (alertText === 'El usuario con correo kevina.moralesc@uqvirtual.edu.co ha sido registrado correctamente') {
+            console.log('La alerta contiene el texto esperado.');
+        } else {
+            console.error('La alerta no contiene el texto esperado.');
+        }
+    } catch (e) {
+        console.log('No se encontró ninguna alerta.');
+    }
+}
+
+
+async function Registro(driver) {
+    console.log('Resgistro...');
+    // Esperar a que el botón "Login" esté presente
+    await driver.wait(until.elementLocated(By.xpath("//button[contains(text(), 'Sign up')]")), 10000);
+    // Hacer clic en el botón "Login"
+    await driver.findElement(By.xpath("//button[contains(text(), 'Sign up')]")).click();
+    // Continuar con el resto de las acciones de inicio de sesión
+    await driver.findElement(By.id('nombre')).sendKeys('kevin Andres ');
+    await driver.findElement(By.id('apellido')).sendKeys('Morales Castaño');
+    await driver.findElement(By.id('id')).sendKeys('1005087691');
+    await driver.findElement(By.id('telefono')).sendKeys('3015402410');
+    await driver.findElement(By.id('email')).sendKeys('kevina.moralesc@uqvirtual.edu.co');
+    await driver.findElement(By.id('password')).sendKeys('123456789');
+    await driver.findElement(By.id('cpassword')).sendKeys('123456789');
+    await driver.findElement(By.xpath("//button[contains(text(), 'Registrarse')]")).click();
+}
+
 async function runSmokeTest() {
     let options = new chrome.Options();
     options.addArguments('--headless');
@@ -49,6 +85,11 @@ async function runSmokeTest() {
         await driver.wait(until.elementLocated(By.css('body')), 10000);
         await login(driver);
         await handleAlertLogin(driver);
+        await driver.quit();
+        await driver.get('https://frontpruebahumo-production.up.railway.app/');
+        await driver.wait(until.elementLocated(By.css('body')), 10000);
+        await Registro(driver);
+        await handleAlertRegistro(driver);
     } catch (error) {
         console.error('Ocurrió un error durante la ejecución de la prueba:', error);
     } finally {
